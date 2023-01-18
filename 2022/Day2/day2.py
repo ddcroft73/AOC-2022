d4cb3f22919c4d207331ecd7966af8241d74f73e
ddcroft73 <gen.disarray73@outlook.com>
2023-01-17 19:40:16 -0500

    Cleaned up day2.py.

--- 2022/Day2/day2.py
+++ 2022/Day2/day2.py
@@ -1,9 +1,6 @@
 import os
+fpath: str = os.getcwd() + '/2022/Day2/input.txt'
 
-fpath: str = os.getcwd() + '/input.txt'
-
-
-# simple translation part one
 def decode_simple(text: str) -> tuple[str]:
     opponent_decode = {'A': 'rock', 'B': 'paper', 'C': 'scissors'}
     player_decode = {'X': 'rock', 'Y': 'paper', 'Z': 'scissors'}
@@ -11,7 +8,6 @@ def decode_simple(text: str) -> tuple[str]:
     my_throw: str = text[2]
     return opponent_decode[op_throw], player_decode[my_throw]
 
-# Decides what the player needs to throw to carry out the strategy, based on the data in input.txt
 def decode_strategy(text: str) -> tuple[str]:    
     input_a: str = text[0]
     input_b: str = text[2]
@@ -25,13 +21,13 @@ def decode_strategy(text: str) -> tuple[str]:
     my_throw_num: str = None                      # This will be the key used in num_to_str to 
                                                   # select the throw as per the strategy guide
      
-    if (elven_strategy == 'X'):  # lose
+    if (elven_strategy == 'X'):  
         my_throw_num = op_throw_num - 1
 
-    if (elven_strategy == 'Y'):  # draw
+    if (elven_strategy == 'Y'):  
         my_throw_num = op_throw_num
 
-    if (elven_strategy == 'Z'):  # win the round.
+    if (elven_strategy == 'Z'):  
         my_throw_num = op_throw_num + 1
 
     if (my_throw_num > 3):
@@ -39,10 +35,10 @@ def decode_strategy(text: str) -> tuple[str]:
     if (my_throw_num < 1):
         my_throw_num = 3
 
-    #my_strat = {'X': 'Lose', 'Y': 'Draw', 'Z': "Win"}
+    #strat = {'X': 'Lose', 'Y': 'Draw', 'Z': "Win"}
     #print('----------------------------------------------------------')
     #print(f'Op played: {op_throw_str}')
-    #print(f'My strategy says I need to: {my_strat[elven_strategy]}')
+    #print(f'My strategy says I need to: {strat[elven_strategy]}')
     #print(f'Therefore, my Choioce  is: { num_to_str[my_throw_num]}')
 
     return opponent_decode[input_a], num_to_str[my_throw_num]
@@ -73,12 +69,12 @@ def get_winner(op_throw: str, pl_throw: str) -> tuple[str]:
 
     for profile in player_wins_profiles:
         if (op_throw == pl_throw):
-            round = 3  # tie
+            round = 3  
             winner = 'Draw'
             break
 
         if (profile == match_result):
-            round = 6  # player won
+            round = 6  
             winner = 'Player'
             break
 
@@ -95,7 +91,7 @@ def part_one():
     weapon_score: int = 0
     round_score: int = 0
     winner: str = None
-
+    
     with open(fpath, 'r') as file:
             games: list[str] = file.readlines()
 
@@ -103,12 +99,10 @@ def part_one():
                 opponent_throw, player_throw = decode_simple(match)
                 round_score, weapon_score, winner = get_winner(opponent_throw, player_throw)
                 total_score += (round_score + weapon_score)
-
             #    print( f'\nMatch: {line_cnt} Part One\n------------------------------------------------------------')
             #    print(f'Player: {player_throw.upper()} Opponent: {opponent_throw.upper()}')
             #    print(f'Score this round: {round_score}, Weapon Score: {weapon_score} \nTotal: {total_score}')
             #    print(f'Winner is: {winner[0]}')
-
     print(f'Part One total score: {total_score}')
 
 def part_two():
@@ -125,12 +119,10 @@ def part_two():
             opponent_throw, player_throw = decode_strategy(match)
             round_score, weapon_score, winner = get_winner(opponent_throw, player_throw)
             total_score += (round_score + weapon_score)
-
            # print( f'\nMatch: {line_cnt} Part Two\n------------------------------------------------------------')
            # print(f'Opponent: {opponent_throw.upper()} Player: {player_throw.upper()}')
            # print(f'Score this round: {round_score}, Weapon Score: {weapon_score} \nTotal: {total_score}\n')
            # print(f'Winner is: {winner[0]}')
-
     print(f'Part Two total score: {total_score}')
 
 
